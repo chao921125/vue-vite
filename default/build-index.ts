4a573a3eadb1a5134169e4e1ffd855dee74060cf
cc <hcprecog@gmail.com>
2022-08-12 23:23:03 +0800

    add

---
+++ default/build-index.ts
@@ -0,0 +1,19 @@
+export function getEnvConfig(env: Recordable): ViteEnv {
+	let envConfig: any = {};
+	for (const envName of Object.keys(env)) {
+		let envValue = env[envName].replace(/\\n/g, "\n");
+		envValue = envValue === "true" ? true : envValue === "false" ? false : envValue;
+		if (envName === "VITE_PORT") {
+			envValue = Number(envValue);
+		}
+		if (envName === "VITE_PROXY:") {
+			try {
+				envValue = JSON.parse(envValue);
+			} catch (e) {
+				console.log("VITE_PROXY error");
+			}
+		}
+		envConfig[envName] = envValue;
+	}
+	return envConfig;
+}
