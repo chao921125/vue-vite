24f836b474f02f58c91dee646479bd98ac716350
hd-huangchao <chao.huang@advokategroup.com>
2022-09-02 15:27:14 +0800

    Update Home.vue

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,7 +5,23 @@
 		<h2 v-show="!isShow" key="2">动画效果2</h2>
 	</transition-group>
 	<div v-html="htmlTest"></div>
-	<div v-for="(item, index) in 100" :key="index">home {{ item }}</div>
+	<el-input
+		v-model="text"
+		maxlength="100"
+		:autosize="{ minRows: 4, maxRows: 8 }"
+		placeholder="Please input"
+		show-word-limit
+		type="textarea"
+	/>
+	<div v-html="textHtml"></div>
+	<el-input
+		v-model="textRe"
+		maxlength="100"
+		:autosize="{ minRows: 4, maxRows: 8 }"
+		placeholder="Please input"
+		show-word-limit
+		type="textarea"
+	/>
 </template>
 
 <script lang="ts">
@@ -22,12 +38,31 @@
 				Utils.Cookies.removeCookie(Utils.Constants.cookieKey.token);
 				router.push({ path: "/login" });
 			};
+
+			const htmlTest = "<div style='color: red'>1231233</div>";
+			const text = ref("");
+			const textHtml = ref("");
+			const textRe = ref("");
+
 			const isShow = ref(false);
 			const changeShow = () => {
 				isShow.value = !isShow.value;
+				textHtml.value = "";
+				console.log(text.value.toString())
+				if (text.value.replace(/\r\n/g, '|rn|').includes("|rn|")) {
+					textHtml.value = text.value.replace(/\r\n/g, '@rn@</br>');
+				}
+				if (text.value.replace(/\n/g, '|n|').includes("|n|")) {
+					textHtml.value = text.value.replace(/\n/g, '@n@</br>');
+				}
+				textHtml.value = textHtml.value.replace(/@n@/g, '');
+
+				textRe.value = textHtml.value.replace(/<\/br>/g, "\r\n");
 			};
-			const htmlTest = "<div style='color: red'>1231233</div>";
 			return {
+				text,
+				textHtml,
+				textRe,
 				isShow,
 				htmlTest,
 				changeShow,
