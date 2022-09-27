18beadfdf22495b8c035693855165df80e8ac2f3
hd-huangchao <chao.huang@advokategroup.com>
2022-09-27 09:57:10 +0800

    uu

--- types/window.d.ts
+++ types/window.d.ts
@@ -4,6 +4,11 @@ declare global {
 		msSaveOrOpenBlob: (blob: Blob, fileName: string) => void;
 		browserLanguage: string;
 	}
+	// 声明文件，定义全局变量。其它 app.config.globalProperties.xxx，使用 getCurrentInstance() 来获取
+	interface Window {
+		nextLoading: boolean;
+	}
+
 }
 
 export {};
