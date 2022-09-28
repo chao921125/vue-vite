0b903e66d0a8cfb32e110c8e06a3124e05f3e6fd
cc <hcprecog@gmail.com>
2022-09-29 01:36:55 +0800

    修改setup

--- src/App.vue
+++ src/App.vue
@@ -4,74 +4,65 @@
 	</el-config-provider>
 </template>
 
-<script lang="ts">
-	import { defineComponent, getCurrentInstance, reactive, onBeforeMount, onMounted, onUnmounted, watch } from "vue";
+<script lang="ts" setup name="App">
+	import { getCurrentInstance, reactive, onBeforeMount, onMounted, onUnmounted, watch } from "vue";
 	import { useRoute } from "vue-router";
 	import Pinia from "@/store";
 	import { useThemeConfig } from "@/store/modules/theme";
 	import Utils from "@/plugins/utils";
 	import { storeToRefs } from "pinia";
 
-	export default defineComponent({
-		name: "App",
-		setup() {
-			const { proxy } = <any>getCurrentInstance();
-			// large / default /small
-			const config = reactive({
-				i18n: import.meta.env.VITE_LOCAL,
-				size: "default",
-				buttonSpace: {
-					autoInsertSpace: false,
-				},
-			});
-
-			const storeThemeConfig = useThemeConfig(Pinia);
-			const { themeConfig } = storeToRefs(storeThemeConfig);
-			onBeforeMount(() => {
-				Utils.setCssCdn();
-				Utils.setJsCdn();
-				if (!Utils.Storages.getLocalStorage(Utils.Constants.storageKey.themeConfig)) {
-					Utils.Storages.setLocalStorage(Utils.Constants.storageKey.themeConfig, themeConfig.value);
-					(config.i18n as unknown as string | null) = themeConfig.value.globalI18n;
-				}
-			});
-
-			onMounted(() => {
-				initData();
-				if (Utils.Storages.getLocalStorage(Utils.Constants.storageKey.themeConfig)) {
-					storeThemeConfig.setThemeConfig(Utils.Storages.getLocalStorage(Utils.Constants.storageKey.themeConfig));
-				}
-				proxy.mittBus.on("getI18nConfig", (local: string) => {
-					(config.i18n as unknown as string | null) = local;
-				});
-				proxy.mittBus.on("getSizeConfig", (size: string) => {
-					config.size = size;
-				});
-			});
-			const initData = () => {
-				config.i18n = Utils.Storages.getLocalStorage(Utils.Constants.storageKey.i18nLocal);
-			};
-
-			onUnmounted(() => {
-				proxy.mittBus.off("getI18nConfig");
-			});
-
-			const route = useRoute();
-			watch(
-				() => route.path,
-				() => {
-					Utils.setTitle();
-				},
-				{
-					deep: true,
-				},
-			);
-
-			return {
-				config,
-			};
+	const { proxy } = <any>getCurrentInstance();
+	// large / default /small
+	const config = reactive({
+		i18n: import.meta.env.VITE_LOCAL,
+		size: "default",
+		buttonSpace: {
+			autoInsertSpace: false,
 		},
 	});
+
+	const storeThemeConfig = useThemeConfig(Pinia);
+	const { themeConfig } = storeToRefs(storeThemeConfig);
+	onBeforeMount(() => {
+		Utils.setCssCdn();
+		Utils.setJsCdn();
+		if (!Utils.Storages.getLocalStorage(Utils.Constants.storageKey.themeConfig)) {
+			Utils.Storages.setLocalStorage(Utils.Constants.storageKey.themeConfig, themeConfig.value);
+			(config.i18n as unknown as string | null) = themeConfig.value.globalI18n;
+		}
+	});
+
+	onMounted(() => {
+		initData();
+		if (Utils.Storages.getLocalStorage(Utils.Constants.storageKey.themeConfig)) {
+			storeThemeConfig.setThemeConfig(Utils.Storages.getLocalStorage(Utils.Constants.storageKey.themeConfig));
+		}
+		proxy.mittBus.on("getI18nConfig", (local: string) => {
+			(config.i18n as unknown as string | null) = local;
+		});
+		proxy.mittBus.on("getSizeConfig", (size: string) => {
+			config.size = size;
+		});
+	});
+	const initData = () => {
+		config.i18n = Utils.Storages.getLocalStorage(Utils.Constants.storageKey.i18nLocal);
+	};
+
+	onUnmounted(() => {
+		proxy.mittBus.off("getI18nConfig");
+	});
+
+	const route = useRoute();
+	watch(
+		() => route.path,
+		() => {
+			Utils.setTitle();
+		},
+		{
+			deep: true,
+		},
+	);
 </script>
 
 <style scoped lang="scss"></style>
