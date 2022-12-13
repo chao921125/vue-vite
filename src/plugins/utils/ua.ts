226511f9d27cabcef7e40d9b85d673da6235719d
cc <hcprecog@gmail.com>
2022-12-13 17:24:17 +0800

    1

---
+++ src/plugins/utils/ua.ts
@@ -0,0 +1,4 @@
+// PC 插件 https://github.com/faisalman/ua-parser-js
+// M 插件 https://github.com/hgoebl/mobile-detect.js/
+// IP 关于IP这一块，建议后端获取
+export default {};
--- src/views/log/login/LoginList.vue
+++ src/views/log/login/LoginList.vue
@@ -79,7 +79,47 @@
 			},
 		];
 	};
+
+	const getUserIP = (onNewIP) => {
+		const MyPeerConnection = window.RTCPeerConnection || window?.mozRTCPeerConnection || window?.webkitRTCPeerConnection;
+
+		const pc = new MyPeerConnection({
+			iceServers: [],
+		});
+		const noop = () => {};
+		const localIPs = {};
+		const ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g;
+		const iterateIP = (ip) => {
+			if (!localIPs[ip]) onNewIP(ip);
+			localIPs[ip] = true;
+		};
+
+		pc.createDataChannel("");
+
+		pc.createOffer()
+			.then((sdp: any) => {
+				sdp.sdp.split("\n").forEach(function (line) {
+					if (line.indexOf("candidate") < 0) return;
+
+					line.match(ipRegex).forEach(iterateIP);
+				});
+
+				pc.setLocalDescription(sdp, noop, noop);
+			})
+			.catch(() => {});
+
+		pc.onicecandidate = (ice: any) => {
+			if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) {
+				return;
+			}
+			ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+		};
+	};
+
 	onMounted(() => {
+		getUserIP((ip) => {
+			console.log(ip);
+		});
 		initData();
 	});
 </script>
