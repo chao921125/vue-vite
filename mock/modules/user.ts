b594879c43ae5339f3b92f116729d015cf3f8649
hd-huangchao <chao.huang@advokategroup.com>
2022-12-27 16:43:40 +0800

    add mock api

--- mock/modules/user.ts
+++ mock/modules/user.ts
@@ -1,1 +1,12 @@
-export default {};
+import { MockMethod } from "vite-plugin-mock";
+export default [
+	{
+		url: "/mock-login", // 注意，这里只能是string格式
+		method: "post",
+		response: () => {
+			return {
+				menusList: [1, 2, 3, 4],
+			};
+		},
+	},
+] as MockMethod[];
