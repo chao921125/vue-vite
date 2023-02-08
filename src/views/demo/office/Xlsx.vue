b1ea1e359e737a303d495b35f120c945ceb5ff75
cc <hcprecog@gmail.com>
2023-02-08 23:05:43 +0800

    update read file

--- src/views/demo/office/Xlsx.vue
+++ src/views/demo/office/Xlsx.vue
@@ -9,6 +9,7 @@
 		<el-link :underline="false" type="primary" class="re-m-l-10">下载</el-link>
 		<el-link :underline="false" type="danger" class="re-m-l-10">删除</el-link>
 	</div>
+	<div v-html="fileObj.fileReader" class="file-reader-box"></div>
 </template>
 
 <script lang="ts" setup name="Xlsx">
@@ -19,6 +20,7 @@
 	const isLoading = ref(false);
 	const fileObj = reactive({
 		fileList: <any>[{ id: "id-xxxxxxx", name: "file.xlsx", url: "https://domain.com/id-xxxxxxx" }],
+		fileReader: "",
 	});
 
 	const initData = () => {
@@ -28,7 +30,38 @@
 			readXlsxLocal();
 		}
 	};
-	const previewFile = () => {};
+	const previewFile = (index: number) => {
+		console.log(index);
+		const file = fileObj.fileList[index];
+		const reader = new FileReader();
+		reader.onload = (e: any) => {
+			const data = e.target.result;
+			const workbook = XLSX.read(data, { type: "array" });
+			const firstSheetName = workbook.SheetNames[0];
+			const worksheet = workbook.Sheets[firstSheetName];
+			const header = getHeaderRow(worksheet);
+			const results = XLSX.utils.sheet_to_html(worksheet);
+			console.log({ header, results });
+			fileObj.fileReader = results;
+		};
+		reader.readAsArrayBuffer(file);
+	};
+	const getHeaderRow = (sheet: any) => {
+		const headers = <any>[];
+		const range = XLSX.utils.decode_range(sheet["!ref"]);
+		let C;
+		const R = range.s.r;
+		/* start in the first row */
+		for (C = range.s.c; C <= range.e.c; ++C) {
+			/* walk every column in the range */
+			const cell = sheet[XLSX.utils.encode_cell({ c: C, r: R })];
+			/* find the cell in the first row */
+			let hdr = "UNKNOWN " + C; // <-- replace with your desired default
+			if (cell && cell.t) hdr = XLSX.utils.format_cell(cell);
+			headers.push(hdr);
+		}
+		return headers;
+	};
 	const fileChange = (file: any) => {
 		if (fileObj.fileList.length) {
 			fileObj.fileList = [...fileObj.fileList, ...file.target.files];
