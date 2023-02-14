d7d33fcc5b7c71ff861cf487b86b1bddc6ef583e
hd-huangchao <chao.huang@advokategroup.com>
2023-02-14 15:00:32 +0800

    update files

---
+++ src/components/back/Index.vue
@@ -0,0 +1,22 @@
+<template>
+	<el-button link @click="goBack" type="primary">back</el-button>
+</template>
+
+<script lang="ts" setup name="Back">
+	const propsData = defineProps({
+		url: {
+			type: String,
+			default: "",
+		},
+	});
+	const router = useRouter();
+	const goBack = () => {
+		if (propsData.url) {
+			router.push({ path: propsData.url });
+		} else {
+			router.back();
+		}
+	};
+</script>
+
+<style scoped lang="scss"></style>
