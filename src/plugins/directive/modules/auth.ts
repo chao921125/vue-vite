160abb2313f9ca1ddd66ce3b2ce82f73091227cf
huangchao <chao.huang@advokategroup.com>
2023-02-23 17:26:20 +0800

    fix: 修改指令集

--- src/plugins/directive/modules/auth.ts
+++ src/plugins/directive/modules/auth.ts
@@ -1,9 +1,11 @@
 import type { Directive, DirectiveBinding } from "vue";
 
-export const auth: Directive = {
+const auth: Directive = {
 	// 在绑定元素的父组件
 	// 及他自己的所有子节点都挂载完成后调用
 	mounted(el: HTMLElement, binding: DirectiveBinding) {
 		console.log(el, binding);
 	},
 };
+
+export default auth;
