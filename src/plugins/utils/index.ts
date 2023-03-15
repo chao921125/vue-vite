895f37c84e99f095c129eedc229aa8ac6783b1fe
huangchao <77830871+hc-advokate@users.noreply.github.com>
2023-03-15 17:15:08 +0800

    fix: 修改手机端判断

--- src/plugins/utils/index.ts
+++ src/plugins/utils/index.ts
@@ -115,10 +115,33 @@ util.urlToObj = (url: string) => {
 };
 
 // 判断手机
-util.isMobile = () => {
-	return navigator.userAgent.match(
-		/(phone|pad|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows phone)/i,
-	);
+util.isMobile = (opts) => {
+	const mobileRE =
+		/(android|bb\d+|meego).+mobile|armv7l|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series[46]0|samsungbrowser.*mobile|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i;
+	const notMobileRE = /CrOS/;
+	const tabletRE = /android|ipad|playbook|silk/i;
+	if (!opts) opts = {};
+	let ua = opts.ua;
+	if (!ua && typeof navigator !== "undefined") ua = navigator.userAgent;
+	if (ua && ua.headers && typeof ua.headers["user-agent"] === "string") {
+		ua = ua.headers["user-agent"];
+	}
+	if (typeof ua !== "string") return false;
+
+	let result = (mobileRE.test(ua) && !notMobileRE.test(ua)) || (!!opts.tablet && tabletRE.test(ua));
+
+	if (
+		!result &&
+		opts.tablet &&
+		opts.featureDetect &&
+		navigator &&
+		navigator.maxTouchPoints > 1 &&
+		ua.indexOf("Macintosh") !== -1 &&
+		ua.indexOf("Safari") !== -1
+	) {
+		result = true;
+	}
+	return result;
 };
 // 判断微信
 util.isWeixin = () => {
