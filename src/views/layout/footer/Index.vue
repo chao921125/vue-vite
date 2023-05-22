24d2479e6c6044f9f9bff34bdeee819fb1f4bedf
huangchao <77830871+hc-advokate@users.noreply.github.com>
2023-05-22 17:41:56 +0800

    fix: 底部文字增加

--- src/views/layout/footer/Index.vue
+++ src/views/layout/footer/Index.vue
@@ -1,5 +1,15 @@
-<template>Footer</template>
+<template>
+	<div class="re-w-fill re-flex-row-center footer-body">
+		<a class="re-ml-20" target="_blank" href="https://beian.miit.gov.cn">京ICP备10036305号-1</a>
+		<a class="re-ml-20" target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11010802022657">京公网安备11000002000001号</a>
+		<span class="re-ml-20">© 2023 Cc</span>
+	</div>
+</template>
 
 <script lang="ts" setup name="Footer"></script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+	.footer-body {
+		font-size: 14px;
+	}
+</style>
