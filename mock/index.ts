9634c2700b4f7b0e9fd520ba8880b14fec3ed45a
huangchao <77830871+hc-advokate@users.noreply.github.com>
2023-05-29 17:39:26 +0800

    fix: 更新mock

--- mock/index.ts
+++ mock/index.ts
@@ -1,19 +1,19 @@
 // @ts-ignore
 import { createProdMockServer } from "vite-plugin-mock/client";
 
-const modules: any = import.meta.glob("./modules/*.(j)?(t)?s", { eager: true, import: "default" });
-const mockModules: any[] = [];
+// 无法使用 import.meta.glob tsconfig配置后依然无效，如果有解决方案的大家可以提供一下
+// const modules: any = import.meta.glob("./modules/*.(j)?(t)?s", { eager: true, import: "default" });
+// const mockModules: any[] = [];
+//
+// Object.keys(modules).forEach((key: string) => {
+// 	if (key.includes("/_")) {
+// 		return false;
+// 	}
+// 	mockModules.push(...modules[key].default);
+// });
 
-Object.keys(modules).forEach((key: string) => {
-	if (key.includes("/_")) {
-		return false;
-	}
-	mockModules.push(...modules[key].default);
-});
-
-// import test from "./modules/test";
-// const mockModules: any[] = [...test];
-console.log(mockModules);
+import test from "./modules/test";
+const mockModules: any[] = [...test];
 
 export const setupProdMockServer = () => {
 	createProdMockServer(mockModules);
