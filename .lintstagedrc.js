809168f5c642a293d91cbd12c047929bb519d656
huangchao <77830871+hc-advokate@users.noreply.github.com>
2023-06-06 15:18:50 +0800

    feat: 修改样式

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,7 @@ module.exports = {
 	"*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
 	"{!(package)*.json,*.code-snippets,.!{npm,browserslist}*rc.{js,cjs,mjs}}": ["prettier --write--parser json"],
 	"package.json": ["prettier --write"],
-	"*.vue": ["stylelint --fix", "eslint --fix", "prettier --write"],
+	"*.vue": ["eslint --fix", "prettier --write"],
 	"*.{css,scss,less,styl,html}": ["stylelint --fix", "prettier --write"],
 	"*.md": ["prettier --write"],
 	"*.js": "eslint --cache --fix",
