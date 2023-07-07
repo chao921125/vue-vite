daf0e3644f82d08e46ea023aac1b58b9b3abe662
huangchao <77830871+hc-advokate@users.noreply.github.com>
2023-07-07 17:04:03 +0800

    fix: 手机端修改

--- src/views/layout/frame/Mobile.vue
+++ src/views/layout/frame/Mobile.vue
@@ -1,7 +1,14 @@
 <template>
-	<router-view></router-view>
+	<van-config-provider :theme="isThemDark ? 'dark' : 'light'">
+		<router-view></router-view>
+	</van-config-provider>
 </template>
 
-<script lang="ts" setup name="LayoutMobile"></script>
+<script lang="ts" setup name="LayoutMobile">
+	import { appStore, getStoreRefs } from "@/store";
+
+	const { themeConfig } = getStoreRefs(appStore.useThemeConfig);
+	const isThemDark = ref(themeConfig.value.isDark);
+</script>
 
 <style scoped lang="scss"></style>
