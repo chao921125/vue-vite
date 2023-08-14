750f55fe446d28c51aee3308a98c8d05e0d60474
huangchao <77830871+hc-advokate@users.noreply.github.com>
2023-08-14 14:21:04 +0800

    feat: 更改模板

--- src/views/TempScriptSetup.vue
+++ src/views/TempScriptSetup.vue
@@ -1,5 +1,22 @@
-<template>body</template>
+<template>
+	<div @click="changeClick">template</div>
+</template>
 
-<script lang="ts" setup name="TempScriptSetup"></script>
+<script lang="ts" setup name="TempScriptSetup">
+	const props = defineProps({
+		name: {
+			required: false,
+			type: String,
+			default: "",
+		},
+	});
+	const emits = defineEmits(["change"]);
+	const changeClick = () => {
+		emits("change", true);
+	};
+	defineExpose({
+		changeClick,
+	});
+</script>
 
 <style scoped lang="scss"></style>
