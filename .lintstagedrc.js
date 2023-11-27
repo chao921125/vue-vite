e00d60ad2ea298ea0ad26111386e2463cb28a989
huangchao <77830871+hc-advokate@users.noreply.github.com>
2023-11-27 17:13:42 +0800

    feat: js to mjs

--- .lintstagedrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	"*.{js,jsx,ts,tsx}": ["eslint --cache --fix", "prettier --write"],
-	"{!(package)*.json,*.code-snippets,.!{npm,browserslist}*rc.{js,cjs,mjs}}": ["prettier --write--parser json"],
-	"*.json": ["prettier --write"],
-	"*.vue": ["eslint --fix", "prettier --write"],
-	"*.{css,scss,less,styl,html}": ["stylelint --cache --fix", "prettier --write"],
-	"*.md": ["prettier --write"],
-};
