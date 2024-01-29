59c903121445cdf4cd778ad3f454399c441b73a4
huangchao <77830871+hc-advokate@users.noreply.github.com>
2024-01-29 16:35:19 +0800

    feat: 增加图片转换工具类，待完善

--- src/plugins/utils/img.ts
+++ src/plugins/utils/img.ts
@@ -1,12 +1,12 @@
 const utils = {
-	blobToDataURL: (file: Blob): Promise<any> => {
+	blobToDataURL: (blob: Blob): Promise<any> => {
 		return new Promise((resolve, reject) => {
 			const reader = new FileReader();
 			reader.onerror = () => {
 				reject("ERR_DOM Error on converting blob to data URL");
 			};
 			reader.onloadend = (e) => resolve((e.target as FileReader).result);
-			reader.readAsDataURL(file);
+			reader.readAsDataURL(blob);
 		});
 	},
 	imageToDataURL: (imgURL: string): Promise<any> => {
@@ -49,7 +49,7 @@ const utils = {
 			return "ERR_DOM Error on converting data URI to blob " + e && e.toString ? e.toString() : e;
 		}
 	},
-	dataURLtoBlobImg: (dataURL: string): Promise<HTMLImageElement> => {
+	dataURLtoImgBlob: (dataURL: string): Promise<HTMLImageElement> => {
 		return new Promise((resolve, reject) => {
 			let canvas = document.createElement("canvas");
 			let img = new Image();
@@ -67,64 +67,34 @@ const utils = {
 			};
 		});
 	},
+	dataURLtoFile: (dataURL: any, fileName: string) => {
+		let arr = dataURL.split(","),
+			mime = arr[0].match(/:(.*?);/)[1];
+		let bStr = atob(arr[1]);
+		let n = bStr.length;
+		let u8arr = new Uint8Array(n);
+
+		while (n--) {
+			u8arr[n] = bStr.charCodeAt(n);
+		}
+
+		return new File([u8arr], fileName, { type: mime });
+	},
+	canvasToFile: (canvas: HTMLCanvasElement, type: string, quality: number): Promise<Blob | null> => {
+		return new Promise((resolve) => canvas.toBlob((blob) => resolve(blob), type, quality));
+	},
 };
 
-const fileToDataURL = (file: Blob): Promise<any> => {
-	return new Promise((resolve) => {
-		const reader = new FileReader();
-		reader.onloadend = (e) => resolve((e.target as FileReader).result);
-		reader.readAsDataURL(file);
-	});
-};
-const dataURLToImage = (dataURL: string): Promise<HTMLImageElement> => {
-	return new Promise((resolve) => {
-		const img = new Image();
-		img.onload = () => resolve(img);
-		img.src = dataURL;
-	});
-};
-const canvasToFile = (canvas: HTMLCanvasElement, type: string, quality: number): Promise<Blob | null> => {
-	return new Promise((resolve) => canvas.toBlob((blob) => resolve(blob), type, quality));
-};
-/**
- * 图片压缩方法
- * @param {Object}  file 图片文件
- * @param {String} type 想压缩成的文件类型
- * @param {Nubmber} quality 压缩质量参数
- * @returns 压缩后的新图片
- */
-export const compressionFile = async (file, type = "image/jpeg", quality = 0.6) => {
-	const fileName = file.name;
-	const canvas = document.createElement("canvas");
-	const context = canvas.getContext("2d") as CanvasRenderingContext2D;
-	const base64 = await fileToDataURL(file);
-	const img = await dataURLToImage(base64);
-	canvas.width = img.width;
-	canvas.height = img.height;
-	context.clearRect(0, 0, img.width, img.height);
-	context.drawImage(img, 0, 0, img.width, img.height);
-	const blob = (await canvasToFile(canvas, type, quality)) as Blob; // quality:0.5可根据实际情况计算
-	const newFile = await new File([blob], fileName, {
-		type: type,
-	});
-	return newFile;
-};
+export const imgConvert = (file: any | File, outputFormat: string = "image/jpeg", callback: any) => {
+	// let blob = new Blob([fd], {type: fd.type});
+	// let reBlob = await heic2any({
+	//   blob: blob, toType: "image/jpeg"
+	// });
+	// // @ts-ignore
+	// let reFile = new File([reBlob], fd.name.split(".")[0] + ".jpeg", { type: 'image/jpeg' });
+	// let newFile = await compressionFile(reFile);
+	// const url = URL.createObjectURL(newFile);
 
-function dataURLtoFile(dataurl, filename) {
-	let arr = dataurl.split(","),
-		mime = arr[0].match(/:(.*?);/)[1];
-	let bstr = atob(arr[1]);
-	let n = bstr.length;
-	let u8arr = new Uint8Array(n);
-
-	while (n--) {
-		u8arr[n] = bstr.charCodeAt(n);
-	}
-
-	return new File([u8arr], filename, { type: mime });
-}
-export const convertImageFile = (file, outputFormat = "image/jpeg", callback) => {
-	debugger;
 	let reader = new FileReader();
 	let img = new Image();
 
@@ -142,7 +112,7 @@ export const convertImageFile = (file, outputFormat = "image/jpeg", callback) =>
 				ctx.drawImage(img, 0, 0, img.width, img.height);
 
 				let newDataUrl = canvas.toDataURL(outputFormat);
-				let newFile = dataURLtoFile(newDataUrl, file.name);
+				let newFile = utils.dataURLtoFile(newDataUrl, file.name);
 				callback(newFile);
 			};
 			img.src = e.target.result;
@@ -151,16 +121,20 @@ export const convertImageFile = (file, outputFormat = "image/jpeg", callback) =>
 	reader.readAsDataURL(file);
 };
 
-export const imgTypeChange = () => {
-	// let blob = new Blob([fd], {type: fd.type});
-	// let reBlob = await heic2any({
-	//   blob: blob, toType: "image/jpeg"
-	// });
-	// // @ts-ignore
-	// let reFile = new File([reBlob], fd.name.split(".")[0] + ".jpeg", { type: 'image/jpeg' });
-	// let newFile = await compressionFile(reFile);
-	// const url = URL.createObjectURL(newFile);
+export const imgCompress = async (file: any, type: string = "image/jpeg", quality: number = 0.6) => {
+	const fileName = file.name;
+	const canvas = document.createElement("canvas");
+	const context = canvas.getContext("2d") as CanvasRenderingContext2D;
+	const base64 = await utils.blobToDataURL(file);
+	const img = await utils.dataURLToImage(base64);
+	canvas.width = img.width;
+	canvas.height = img.height;
+	context.clearRect(0, 0, img.width, img.height);
+	context.drawImage(img, 0, 0, img.width, img.height);
+	const blob = (await utils.canvasToFile(canvas, type, quality)) as Blob; // quality:0.5可根据实际情况计算
+	return new File([blob], fileName, {
+		type: type,
+	});
 };
-export const imgCompress = () => {};
 
-export default utils;
+export default { imgConvert, imgCompress, utils };
