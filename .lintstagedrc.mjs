ac2184f90b623b9ded752b35a18bf943b2c4459e
huangchao <77830871+hc-advokate@users.noreply.github.com>
2024-02-27 18:20:57 +0800

    fix: update lint-staged

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,8 +1,8 @@
 export default {
-	"*.{js,jsx,ts,tsx,cjs,mjs}": ["eslint --cache --fix", "prettier --write"],
+	"*.{ts,mts,cts,tsx,js,mjs,cjs,jsx}": ["eslint --cache --fix", "prettier --write"],
 	"{!(package)*.json,*.code-snippets,.!{npm,browserslist}*rc.{js,cjs,mjs}}": ["prettier --write--parser json"],
 	"*.json": ["prettier --write"],
 	"*.vue": ["eslint --fix", "prettier --write"],
 	"*.{css,scss,less,styl,html}": ["stylelint --cache --fix", "prettier --write"],
-	"*.md": ["prettier --write"],
+	"*.{md, mdx}": ["prettier --write"],
 };
