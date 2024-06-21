f32e2da609dd0d7d7089bcd1277920bcd0db2c02
cc <chao921125@sina.com>
2024-06-21 22:35:28 +0800

    feat: 自定义回到指定位置

---
+++ src/views/example/BackPosition.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>回退旧位置</div>
+</template>
+
+<script lang="ts" setup name="">
+	const placeScroll = ref();
+	onBeforeRouteLeave((to, from, next) => {
+		placeScroll.value = document.documentElement.scrollTop;
+		next();
+	});
+	onActivated(() => {
+		nextTick(() => {
+			if (placeScroll.value) {
+				setTimeout(() => {
+					document.documentElement.scrollTo(0, placeScroll.value);
+					document.body.scrollTo(0, placeScroll.value);
+				}, 100);
+			}
+		});
+	});
+</script>
+
+<style scoped lang="scss"></style>
