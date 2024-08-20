a2667c72b668aa7e55c2a4cb90db3ea5a8f7847c
hd-huangchao <chao.huang@advokategroup.com>
2024-08-20 11:20:44 +0800

    feat: up pkg

---
+++ src/views/demo/slide/SwiperNew.vue
@@ -0,0 +1,18 @@
+<template>
+	<swiper-container>
+		<swiper-slide>Slide 1</swiper-slide>
+		<swiper-slide>Slide 2</swiper-slide>
+		<swiper-slide>Slide 3</swiper-slide>
+	</swiper-container>
+</template>
+
+<script lang="ts" setup name="">
+	import { register } from "swiper/element";
+	// import { Navigation, Pagination } from 'swiper/modules';
+
+	onMounted(() => {
+		register();
+	});
+</script>
+
+<style scoped lang="scss"></style>
