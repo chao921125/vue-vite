48bb3920fbe68479da87ec158c3fc442061fe1f0
hd-huangchao <chao.huang@advokategroup.com>
2024-08-28 16:37:34 +0800

    feat: ad doc

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
+// @see https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
 });
