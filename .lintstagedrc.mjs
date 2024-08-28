eb43e493946e3eb67bb16b5aa6d80c918a478a0f
hc precog <chao921125@sina.com>
2024-08-28 19:48:43 +0800

    feat: 修改配置

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,5 @@
 export default {
-	"*.{ts,mts,cts,tsx,js,mjs,cjs,jsx,vue}": ["prettier --write", "eslint --cache --fix"],
+	"*.{ts,mts,cts,tsx,js,mjs,cjs,jsx,vue}": ["prettier --write"],
 	"{!(package)*.json,*.code-snippets,.!{npm,browserslist}*rc.{js,cjs,mjs}}": ["prettier --write--parser json"],
 	"*.json": ["prettier --write"],
 	"*.{css,scss,less,styl,html}": ["prettier --write", "stylelint --cache --fix"],
