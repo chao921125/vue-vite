391ed7bdfc34602aa78457641ceef30a5e631285
hc precog <chao921125@sina.com>
2024-09-01 16:05:04 +0800

    feat: 基础架构搭建完毕

---
+++ src/plugins/utils/index.js
@@ -0,0 +1,155 @@
+import { nextTick } from "vue";
+import Router from "@/router";
+import { getStoreRefs, appStore } from "@/store";
+import I18n from "@/plugins/i18n";
+import ThemeConfig from "@/config/themeConfig";
+
+const util = {};
+
+/**
+ * @description 更新标题
+ * @param titleText
+ */
+util.setTitle = async () => {
+	const { themeConfig } = getStoreRefs(appStore.useThemeConfig);
+	const globalTitle = themeConfig.value.globalTitle;
+	await nextTick(() => {
+		let title = "";
+		title = setTitleI18n(Router.currentRoute.value);
+		window.document.title = `${title}` || globalTitle;
+	});
+};
+
+util.tagsName = (value) => {
+	return setTitleI18n(value);
+};
+
+const setTitleI18n = (value) => {
+	// @ts-ignore
+	let tagsViewName = import.meta.env.VITE_TITLE;
+	const { query, params, meta } = value;
+	if (query?.tagsViewName || params?.tagsViewName) {
+		if (ThemeConfig.i18nKey.test(query?.tagsViewName) || ThemeConfig.i18nKey.test(params?.tagsViewName)) {
+			// 国际化
+			const urlTagsParams = (query?.tagsViewName && JSON.parse(query?.tagsViewName)) || (params?.tagsViewName && JSON.parse(params?.tagsViewName));
+			tagsViewName = urlTagsParams[I18n.global.locale.value];
+		} else {
+			// 非国际化
+			tagsViewName = query?.tagsViewName || params?.tagsViewName;
+		}
+	} else {
+		// 非自定义 tagsView 名称
+		tagsViewName = I18n.global.t(meta.title);
+	}
+	return tagsViewName;
+};
+
+/**
+ * 设置cdn
+ */
+// 字体图标 url
+const cssCdnUrlList = [];
+// 第三方 js url
+const jsCdnUrlList = [];
+// 动态批量设置字体图标
+util.setCssCdn = () => {
+	if (cssCdnUrlList.length <= 0) return false;
+	cssCdnUrlList.map((v) => {
+		let link = document.createElement("link");
+		link.rel = "stylesheet";
+		link.href = v;
+		link.crossOrigin = "anonymous";
+		document.getElementsByTagName("head")[0].appendChild(link);
+	});
+};
+// 动态批量设置第三方js
+util.setJsCdn = () => {
+	if (jsCdnUrlList.length <= 0) return false;
+	jsCdnUrlList.map((v) => {
+		let link = document.createElement("script");
+		link.src = v;
+		document.body.appendChild(link);
+	});
+};
+
+/**
+ * @description 打开新页面
+ * @param {String} url 地址
+ */
+util.open = (url) => {
+	if (!url) return false;
+	let a = document.createElement("a");
+	a.setAttribute("href", url);
+	a.setAttribute("target", "_blank");
+	a.setAttribute("id", "open_window_blank");
+	document.body.appendChild(a);
+	a.click();
+	document.body.removeChild(document.getElementById("open_window_blank"));
+};
+
+/**
+ * http url to object
+ * {
+ *   "url": "string"
+ * }
+ * */
+util.urlToObj = (url) => {
+	let obj = {};
+	if (!url) return obj;
+	// @ts-ignore
+	url.replace(/([^?=&#]+)=([^?=&#]+)/g, (_, key, value) => (obj[key] = value));
+	// @ts-ignore
+	url.replace(/#([^?=&#]+)/g, (_, hash) => (obj["HASH"] = hash));
+	return obj;
+};
+
+/**
+ * navigator.userAgent 不可靠容易被修改 /Mobi|Android|iPhone/i.test(navigator.userAgent)
+ */
+export function isMobileTouch1() {
+	return "ontouchstart" in document.documentElement;
+}
+// 另一种写法
+export function isMobileTouch2() {
+	try {
+		document.createEvent("TouchEvent");
+		return true;
+	} catch (e) {
+		return false;
+	}
+}
+
+/**
+ * 判断手机
+ * https://github.com/matthewhudson/current-device
+ * @param opts
+ */
+util.isMobile = (opts) => {
+	const mobileRE = /(android|bb\d+|meego).+mobile|armv7l|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series[46]0|samsungbrowser.*mobile|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i;
+	const notMobileRE = /CrOS/;
+	const tabletRE = /android|ipad|playbook|silk/i;
+	if (!opts) opts = {};
+	let ua = opts.ua;
+	if (!ua && typeof navigator !== "undefined") ua = navigator.userAgent;
+	if (ua && ua.headers && typeof ua.headers["user-agent"] === "string") {
+		ua = ua.headers["user-agent"];
+	}
+	if (typeof ua !== "string") return false;
+
+	let result = (mobileRE.test(ua) && !notMobileRE.test(ua)) || (!!opts.tablet && tabletRE.test(ua));
+
+	if (!result && opts.tablet && opts.featureDetect && navigator && navigator.maxTouchPoints > 1 && ua.indexOf("Macintosh") !== -1 && ua.indexOf("Safari") !== -1) {
+		result = true;
+	}
+	return result;
+};
+/**
+ * 判断微信
+ */
+util.isWeixin = () => {
+	const ua = navigator.userAgent.toLowerCase();
+	// @ts-ignore
+	return ua.match(/MicroMessenger/i) === "micromessenger";
+};
+
+export default util;
---
+++ src/plugins/utils/ip.js
@@ -0,0 +1,204 @@
+// 若非获取客户端本地IP，直接通过NG配置，在服务端获取即可
+import Fetch from "@/plugins/http/fetch";
+
+export const getLocalIpList = (callback) => {
+	let ip_dups = {};
+	// @ts-ignore
+	let RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+	// 如果不存在则使用一个iframe绕过
+	if (!RTCPeerConnection) {
+		// 因为这里用到了iframe，所以在调用这个方法的script上必须有一个iframe标签
+		// <iframe id="iframe" sandbox="allow-same-origin"></iframe>
+		let iframe = document.createElement("iframe");
+		// @ts-ignore
+		let win = iframe.contentWindow;
+		// @ts-ignore
+		RTCPeerConnection = win.RTCPeerConnection || win.mozRTCPeerConnection || win.webkitRTCPeerConnection;
+	}
+	// let useWebKit = !!window.webkitRTCPeerConnection;
+	let mediaConstraints = {
+		optional: [{ RtpDataChannels: true }],
+	};
+	// 这里就是需要的ICEServer了
+	let servers = {
+		iceServers: [{ urls: "stun:stun.services.mozilla.com" }, { urls: "stun:stun.l.google.com:19302" }],
+	};
+	// @ts-ignore
+	let pc = new RTCPeerConnection(servers, mediaConstraints);
+	const handleCandidate = (candidate) => {
+		// /([0-9]{1,3}(\.[0-9]{1,3}){3}|([a-f0-9]{1,4}((:[a-f0-9]{1,4}){7}|:+[a-f0-9]{1,4}){6}))/g
+		let ip_regex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g;
+		let hasIp = ip_regex.exec(candidate);
+		if (hasIp) {
+			// candidate.match(ip_regex)[1];
+			// @ts-ignore
+			let ip_addr = ip_regex.exec(candidate)[1];
+			if (ip_dups[ip_addr] === undefined) callback(ip_addr);
+			ip_dups[ip_addr] = true;
+		}
+	};
+	pc.createDataChannel("");
+	//创建一个SDP(session description protocol)会话描述协议 是一个纯文本信息 包含了媒体和网络协商的信息
+	pc.createOffer(
+		(result) => {
+			pc.setLocalDescription(
+				result,
+				() => {},
+				() => {},
+			);
+		},
+		() => {},
+	);
+	setTimeout(() => {
+		// @ts-ignore
+		let lines = pc.localeDescription.sdp.split("\n");
+		lines.forEach((line) => {
+			if (line.indexOf("a=candidate:") === 0) handleCandidate(line);
+		});
+	}, 1000);
+	// 网络协商的过程
+	pc.onicecandidate = (ice) => {
+		if (ice.candidate) {
+			handleCandidate(ice.candidate.candidate);
+		}
+	};
+};
+export const getLocalIPs = () => {
+	// @ts-ignore
+	let myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+	// RTCPeerConnection是WebRTC用于构建点对点之间稳定、高效的流传输的组件。兼容火狐、谷歌等
+	let pc = new myPeerConnection({
+		// 创建点对点连接的RTCPeerConnection的实例
+		// @ts-ignore
+		iceServers: [{ url: "stun:stun.services.mozilla.com" }, { url: "stun:stun.l.google.com:19302" }],
+	}); // webRTC使用了ICE协议框架，包括STUN 和 TURN两个协议。我这里连接的是STUN协议服务器。STUN Server的作用是接受客户端的请求，并且把客户端的公网IP、Port封装到ICECandidate中。
+	let noop = function () {};
+	let localIPs = {}; // 记录有没有被调用到onNewIP这个listener上
+	let ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g;
+
+	function ipIterate(ip) {
+		// @ts-ignore
+		if (!localIPs[ip]) onNewIP(ip);
+		localIPs[ip] = true;
+	}
+	pc.createDataChannel(""); // 创建数据信道
+	pc.createOffer().then(function (sdp) {
+		// @ts-ignore
+		sdp.sdp.split("\n").forEach(function (line) {
+			if (line.indexOf("candidate") < 0) return;
+			// @ts-ignore
+			line.match(ipRegex).forEach(ipIterate);
+		});
+		pc.setLocalDescription(sdp, noop, noop);
+	});
+	pc.onicecandidate = function (ice) {
+		//listen for candidate events
+		if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+		// @ts-ignore
+		ice.candidate.candidate.match(ipRegex).forEach(ipIterate);
+	};
+};
+
+// Test
+// getLocalIpList((ip) => {console.log(ip);});
+
+// TODO https://github.com/ihmily/ip-info-api
+// http://ip-api.com/json/
+
+// http://www.net.cn/static/customercare/yourip.asp
+// https://www.ip.cn/
+// https://www.ip138.com/
+// https://www.hao7188.com/
+// https://www.lingfengyun.com/
+
+/**
+ * mode: "no-cors"
+ * @param url
+ */
+export const getIp = (url) => {
+	return Fetch.request(url, {}, { method: "GET" });
+};
+
+/**
+ * 只能通过 ip 获取信息
+ * @param url
+ * @param ip
+ */
+export const getInfoByIp = (url, ip) => {
+	return Fetch.request(`${url}${ip || ""}`, {}, { method: "GET" });
+};
+
+/**
+ * 获取 ip 信息，或者，根据 ip 地址获取信息
+ * @param url
+ * @param ip
+ */
+export const getIpInfo = (url, ip) => {
+	return Fetch.request(`${url}${ip || ""}`, {}, { method: "GET" });
+};
+
+/**
+ * 获取真实的 ip 信息，或者，根据 ip 地址获取信息
+ * @param url
+ * @param ip
+ */
+export const getRealIpInfo = (url, ip) => {
+	return Fetch.request(`${url}${ip || ""}`, {}, { method: "GET" });
+};
+
+/**
+ * 获取 IP 为代理后的地址
+ * @param url
+ */
+export const getProxyIpInfo = (url) => {
+	return Fetch.request(url, {}, { method: "GET" });
+};
+
+/**
+ * 需要单独处理的
+ */
+
+// https://ifconfig.me/
+// https://ifconfig.es/
+// https://ipcalf.com/
+// https://tnx.nl/ip
+
+export const getIpChaXun = () => {
+	let url = `https://${new Date().getFullYear()}.ipchaxun.com/`;
+	return Fetch.request(url, {}, { method: "GET" });
+};
+
+export const getIp138 = () => {
+	let url = `https://${new Date().getFullYear()}.ip138.com`;
+	let data;
+	let xmlHttpRequest;
+	if (window.ActiveXObject) {
+		xmlHttpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+	} else if (window.XMLHttpRequest) {
+		xmlHttpRequest = new XMLHttpRequest();
+	}
+	xmlHttpRequest.onreadystatechange = function () {
+		if (xmlHttpRequest.readyState == 4) {
+			if (xmlHttpRequest.status == 200) {
+				data = xmlHttpRequest.responseText;
+			} else {
+				alert("error:HTTP状态码为:" + xmlHttpRequest.status);
+			}
+		}
+	};
+	xmlHttpRequest.open("get", url, false);
+	xmlHttpRequest.send(null);
+	let datalist = data.split("\n");
+	let patt = [/[0-9]+.[0-9]+.[0-9]+.[0-9]+/, /来自/, []];
+	// @ts-ignore
+	for (let i in datalist) {
+		// @ts-ignore
+		if (patt[0].test(datalist[i]) && patt[1].test(datalist[i])) {
+			// @ts-ignore
+			patt[2].push(patt[0].exec(datalist[i])[0]);
+			// @ts-ignore
+			patt[2].push(datalist[i].substr(patt[1].exec(datalist[i]).index + 3));
+		}
+	}
+	return patt[2];
+};
