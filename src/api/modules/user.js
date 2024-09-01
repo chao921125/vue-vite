bf5ceffee59e56f5e6405d8edae0b28ca5b2badd
hc precog <chao921125@sina.com>
2024-09-01 15:59:28 +0800

    feat: 基础架构搭建完毕

---
+++ src/api/modules/user.js
@@ -0,0 +1,19 @@
+import Axios from "@/plugins/http";
+import AxiosConfig from "@/config/httpConfig";
+
+export default {
+	loginUser: (data = {}) => {
+		return Axios({
+			url: `${AxiosConfig.baseUrl}/login`,
+			method: "POST",
+			data,
+		});
+	},
+	queryUserInfo: (data = {}) => {
+		return Axios({
+			url: `${AxiosConfig.baseUrl}/user-info`,
+			method: "GET",
+			data,
+		});
+	},
+};
