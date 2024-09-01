1cb88d303a0744e70170aaaceaa9949e920a3b9a
hc precog <chao921125@sina.com>
2024-09-01 16:08:06 +0800

    feat: 基础架构搭建完毕

---
+++ src/plugins/echarts.js
@@ -0,0 +1,9 @@
+import * as echarts from "echarts/core";
+
+export default function Echarts(dom, options = {}) {
+	const htmlDom = (dom && document.querySelector(`#${dom}`)) || document.querySelector("#echarts");
+	let myEcharts = echarts.init(htmlDom);
+	myEcharts.clear();
+	myEcharts.setOption(options);
+	myEcharts.resize();
+}
