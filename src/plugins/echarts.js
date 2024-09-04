ae0f138ce8f1831998d5d81b51ed150f82b10553
hd-huangchao <chao.huang@advokategroup.com>
2024-09-04 15:24:34 +0800

    feat: 遵循严格的 js

--- src/plugins/echarts.js
+++
@@ -1,9 +0,0 @@
-import * as echarts from "echarts/core";
-
-export default function Echarts(dom, options = {}) {
-	const htmlDom = (dom && document.querySelector(`#${dom}`)) || document.querySelector("#echarts");
-	const myEcharts = echarts.init(htmlDom);
-	myEcharts.clear();
-	myEcharts.setOption(options);
-	myEcharts.resize();
-}
