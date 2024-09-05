c9083ca78198deb39d6d88096b53c909fcffe118
hd-huangchao <chao.huang@advokategroup.com>
2024-09-05 13:52:01 +0800

    feat: add js st

--- src/plugins/px2rem/index.js
+++ src/plugins/px2rem/index.js
@@ -2,9 +2,9 @@ const baseSize = 16;
 // 设置 rem 函数
 function setRem() {
 	// 320 默认大小16px; 320px = 20rem ;每个元素px基础上/16
-	let htmlWidth = document.documentElement.clientWidth || document.body.clientWidth;
+	const htmlWidth = document.documentElement.clientWidth || document.body.clientWidth;
 	//得到html的Dom元素
-	let htmlDom = document.getElementsByTagName("html")[0];
+	const htmlDom = document.getElementsByTagName("html")[0];
 	//设置根元素字体大小
 	htmlDom.style.fontSize = htmlWidth / baseSize + "px";
 }
@@ -12,5 +12,8 @@ function setRem() {
 setRem();
 // 改变窗口大小时重新设置 rem
 window.onresize = function () {
+	setRem();
+};
+window.onpageshow = function () {
 	setRem();
 };
