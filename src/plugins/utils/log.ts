02ffdde37bb87e87d2b01b6472e35c4f5af635d6
hd-huangchao <chao.huang@advokategroup.com>
2024-09-11 15:19:53 +0800

    feat: up

--- src/plugins/utils/log.ts
+++ src/plugins/utils/log.ts
@@ -41,7 +41,7 @@ function colorful(textArr) {
 	);
 }
 
-log.capsule = function (title, info, type = "primary") {
+log.capsule = (title, info, type = "primary") => {
 	console.log(`%c ${title} %c ${info} %c`, "background:#35495E; padding: 1px; border-radius: 3px 0 0 3px; color: #fff;", `background:${typeColor(type)}; padding: 1px; border-radius: 0 3px 3px 0;  color: #fff;`, "background:transparent");
 };
 
