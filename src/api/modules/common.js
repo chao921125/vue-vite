c4b6eb0c387f481a13edf268e3ee708677e064a3
hd-huangchao <77830871+hc-advokate@users.noreply.github.com>
2024-09-11 15:44:55 +0800

    Update common.js

--- src/api/modules/common.js
+++ src/api/modules/common.js
@@ -32,7 +32,7 @@ export default {
 			data,
 		});
 	},
-	queryAddressByIp: (data = {}) => {
+	queryAddressByIp: (data = { ip: "" }) => {
 		return Axios({
 			url: `${AxiosConfig.ipUrl}/index?ip=${data.ip}&type=0`,
 			method: "GET",
