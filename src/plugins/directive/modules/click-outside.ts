508b6039424351c324a60dfc2551bf98bf006a76
hc precog <hcprecog@gmail.com>
2025-04-23 13:34:15 +0800

    feat: add click-outside

---
+++ src/plugins/directive/modules/click-outside.ts
@@ -0,0 +1,14 @@
+export const clickOutside = {
+	mounted(el, binding) {
+		const handler = (e) => {
+			if (el.contains(e.target)) {
+				return false;
+			}
+			binding.value(e);
+		};
+		document.addEventListener("click", handler);
+	},
+	unmounted(el) {
+		document.removeEventListener("click", el);
+	},
+};
