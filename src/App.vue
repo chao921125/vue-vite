12dfda555b0212dd0049e83f70fabbef51bd711c
hc precog <hcprecog@gmail.com>
2025-05-12 22:59:22 +0800

    style: format

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
-	import { onMounted, ref } from "vue";
-	import HelloWorld from "./components/HelloWorld.vue";
+import { onMounted, ref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
-	const isLoadFont = ref(false);
-	onMounted(() => {
-		const fontFaces = document.fonts.values();
-		let item = fontFaces.next();
-		let count = 0;
-		while (!item.done) {
-			// let ff = item.value;
-			item = fontFaces.next();
-			count += 1;
-		}
-		document.fonts.ready.then(() => {
-			isLoadFont.value = document.fonts.size === count;
-		});
+const isLoadFont = ref(false);
+onMounted(() => {
+	const fontFaces = document.fonts.values();
+	let item = fontFaces.next();
+	let count = 0;
+	while (!item.done) {
+		// let ff = item.value;
+		item = fontFaces.next();
+		count += 1;
+	}
+	document.fonts.ready.then(() => {
+		isLoadFont.value = document.fonts.size === count;
 	});
+});
 </script>
 
 <template>
