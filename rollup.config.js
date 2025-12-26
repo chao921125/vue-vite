616f595b7b19aca2b96568070726b5f30ac5708a
hc precog <hcprecog@gmail.com>
2025-12-26 17:31:38 +0800

    feat: eslint => oxc

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,11 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-	input: "main.ts",
-	output: {
-		file: "bundle.js",
-		format: "iife",
-		name: "MyModule",
-	},
-	plugins: [commonjs(), resolve()],
+  input: "main.ts",
+  output: {
+    file: "bundle.js",
+    format: "iife",
+    name: "MyModule",
+  },
+  plugins: [commonjs(), resolve()],
 };
